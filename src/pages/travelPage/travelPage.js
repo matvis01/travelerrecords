2a440a2078939e72a8162462840c64f697fc483f
matvis01 <mati.siwy@op.pl>
2023-05-16 17:58:02 +0200

    kind of works without backend

--- src/pages/travelPage/travelPage.js
+++ src/pages/travelPage/travelPage.js
@@ -31,9 +31,9 @@ export default function TravelPage(props) {
   useEffect(() => {
     changeAdding()
 
-    if (!localStorage.getItem("token")) {
-      navigate("/")
-    }
+    // if (!localStorage.getItem("token")) {
+    //   navigate("/")
+    // }
     async function fetchData() {
       try {
         const res = await api.get(
